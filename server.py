7dd4ae8f9460ca3f73f1b67d8e2a349b59902386
Panos-Jr <panos.etosjr@gmail.com>
2026-04-09 01:43:11 +0100

    add favicon

--- server.py
+++ server.py
@@ -324,7 +324,7 @@ def dashboard():
     html = f"""<!DOCTYPE html>
 <html lang="en">
 <head>
-<link rel="icon" type="image/png" href="/favicon.ico">
+<link rel="icon" type="image/png" href="/anchor.png">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Depth Server Controller</title>
