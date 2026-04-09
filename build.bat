529dc0850c42c144230328efa2547d731bcec4b8
Panos-Jr <panos.etosjr@gmail.com>
2026-04-09 01:43:00 +0100

    include anchor.png as exe icon

--- build.bat
+++ build.bat
@@ -1,14 +1,14 @@
 @echo off
 echo Installing dependencies...
 pip install flask waitress pystray pillow pyinstaller
-
 echo Building executable...
 pyinstaller ^
   --onefile ^
   --noconsole ^
   --name "DepthController" ^
+  --icon "anchor.png" ^
   --add-data "server.py;." ^
+  --add-data "anchor.png;." ^
   server_tray.py
-
 echo Done. Executable is in dist\DepthController.exe
 pause
